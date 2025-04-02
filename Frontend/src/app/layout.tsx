2082cd05a865dbc7547dc23c7fbde48e7916ba70
NamiNaone99 <nongft09090@gmail.com>
2025-04-02 18:43:53 +0700

    minor change name

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TopMenu from "@/components/TopMenu";
-import {getServerSession} from 'next-auth/next'
-import {authOptions} from './api/auth/[...nextauth]/authOptions'
-import NextAuthProvider from '@/providers/NextAuthProvider'
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]/authOptions";
+import NextAuthProvider from "@/providers/NextAuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anakawee888",
-  description: "Created by DeepseekxChatGPT Group",
+  title: "Space Co-Working",
+  description: "Created by HalloTestTest Group",
 };
 
 export default async function RootLayout({
@@ -23,8 +23,8 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthProvider session={nextAuthSession}>
-        <TopMenu/>
-        {children}
+          <TopMenu />
+          {children}
         </NextAuthProvider>
       </body>
     </html>
