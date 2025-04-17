45d6cbcf46dd18fb76eb7b869571186a7568affa
NamiNaone99 <nongft09090@gmail.com>
2025-04-17 10:07:45 +0700

    add chk ban import

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -5,7 +5,7 @@ const {getAllUsers} = require('../controllers/user');
 const {protect, authorize} = require('../middleware/auth');
 
 const router = express.Router({ mergeParams: true });
-
+const {checkBanned} = require('../middleware/banning');
 
 router.route('/')
     .get(protect,checkBanned, getAllUsers)
