57e84e0710cca36d8ab84f4c248424778b486c45
Khannapong Wangkrasaer <khannpwks173@outlook.com>
2025-04-22 12:56:59 +0700

    Add function to update when delete a review

--- Backend/utils/coworkingstats.js
+++ Backend/utils/coworkingstats.js
@@ -25,4 +25,35 @@ const createAverageRating = async (coWorkingId) => {
     return { averageRating, reviewCount };
 };
 
-module.exports = { createAverageRating };
+const updateDeletedAverageRating = async (coWorkingId, reviewId) => {
+   
+    const stats = await CoWorkingStats.findOne({ CoWorkingId: coWorkingId });
+
+    if (!stats || stats.reviewCount === 0) {
+        return { averageRating: 0, reviewCount: 0 };
+    }
+
+    const review = await Review.findById(reviewId);
+
+    if (!review) {
+        throw new Error('Review not found');
+    }
+
+    const newReviewCount = stats.reviewCount - 1;
+
+    let newAverage = 0;
+
+    if (newReviewCount > 0) {
+        newAverage = ((stats.averageRating * stats.reviewCount) - review.rating) / newReviewCount;
+    }
+
+    await CoWorkingStats.findOneAndUpdate(
+        { CoWorkingId: coWorkingId },
+        { averageRating: newAverage, reviewCount: newReviewCount },
+        { new: true }
+    );
+
+    return { averageRating: newAverage, reviewCount: newReviewCount };
+};
+
+module.exports = { createAverageRating , updateDeletedAverageRating};
