6a1c882323c9e63af2177d1b9ea8cf69da1451cc
Khannapong Wangkrasaer <khannpwks173@outlook.com>
2025-04-22 13:56:56 +0700

    Add function to call when user add,update for average rating

--- Backend/utils/coworkingstats.js
+++ Backend/utils/coworkingstats.js
@@ -2,6 +2,7 @@
 const Review = require('../models/Review');
 const CoWorkingStats = require('../models/CoWorkingStats');
 
+//if no avg rn
 const createAverageRating = async (coWorkingId) => {
     const result = await Review.aggregate([
         { $match: { CoWorkingId: coWorkingId } },
@@ -25,6 +26,7 @@ const createAverageRating = async (coWorkingId) => {
     return { averageRating, reviewCount };
 };
 
+//if user call delete review
 const updateDeletedAverageRating = async (coWorkingId, reviewId) => {
    
     const stats = await CoWorkingStats.findOne({ CoWorkingId: coWorkingId });
@@ -56,4 +58,64 @@ const updateDeletedAverageRating = async (coWorkingId, reviewId) => {
     return { averageRating: newAverage, reviewCount: newReviewCount };
 };
 
-module.exports = { createAverageRating , updateDeletedAverageRating};
+//if user call add review
+const updateAddedAverageRating = async (coWorkingId, reviewId) => {
+   
+    const stats = await CoWorkingStats.findOne({ CoWorkingId: coWorkingId });
+
+    if (!stats || stats.reviewCount === 0) {
+        return { averageRating: 0, reviewCount: 0 };
+    }
+
+    const review = await Review.findById(reviewId);
+
+    if (!review) {
+        throw new Error('Review not found');
+    }
+
+    const newReviewCount = stats.reviewCount + 1;
+
+    let newAverage = 0;
+
+    if (newReviewCount > 0) {
+        newAverage = ((stats.averageRating * stats.reviewCount) + review.rating) / newReviewCount;
+    }
+
+    await CoWorkingStats.findOneAndUpdate(
+        { CoWorkingId: coWorkingId },
+        { averageRating: newAverage, reviewCount: newReviewCount },
+        { new: true }
+    );
+
+    return { averageRating: newAverage, reviewCount: newReviewCount };
+};
+
+//if user call update review
+const updateEditedAverageRating = async (coWorkingId, reviewId, oldRating) => {
+   
+    const stats = await CoWorkingStats.findOne({ CoWorkingId: coWorkingId });
+
+    if (!stats || stats.reviewCount === 0) {
+        return { averageRating: 0, reviewCount: 0 };
+    }
+
+    const review = await Review.findById(reviewId);
+
+    if (!review) {
+        throw new Error('Review not found');
+    }
+
+    let newAverage = 0;
+
+    newAverage = ( (stats.averageRating * stats.reviewCount) + (review.rating - oldRating) ) / stats.reviewCount;
+
+    await CoWorkingStats.findOneAndUpdate(
+        { CoWorkingId: coWorkingId },
+        { averageRating: newAverage, reviewCount: newReviewCount },
+        { new: true }
+    );
+
+    return { averageRating: newAverage, reviewCount: newReviewCount };
+};
+
+module.exports = { createAverageRating , updateDeletedAverageRating, updateAddedAverageRating, updateEditedAverageRating};
